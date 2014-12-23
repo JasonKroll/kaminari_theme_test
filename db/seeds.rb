f594b24791a30592d4a412a16db0e7efcd91925e
Jason Kroll <krolly@gmail.com>
2014-12-24 08:10:37 +1000

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'securerandom'
+150.times do
+	Post.create(
+		title: "Post #{SecureRandom.hex(4)}",
+		author: SecureRandom.hex(8),
+		body:  SecureRandom.hex(32)
+	)
+end
